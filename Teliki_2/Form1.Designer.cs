71d52544c3fbed2c3c0f6172c2926dbf9708d288
root <root@DESKTOP-2M2HDD8.localdomain>
2019-01-23 10:41:32 +0200

    Add Project

---
+++ Teliki_2/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Teliki_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.label_u = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.game_name = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.username_show = new System.Windows.Forms.Label();
+            this.ImageChoose = new System.Windows.Forms.OpenFileDialog();
+            this.counter = new System.Windows.Forms.Timer(this.components);
+            this.image_show = new System.Windows.Forms.Timer(this.components);
+            this.image_destroy = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(916, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.fileToolStripMenuItem.Text = "Settings";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.openToolStripMenuItem.Text = "Choose Images";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // timer
+            // 
+            this.timer.AutoSize = true;
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.timer.Location = new System.Drawing.Point(849, 5);
+            this.timer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(35, 20);
+            this.timer.TabIndex = 1;
+            this.timer.Text = "0 s";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(779, 5);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time:";
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.MintCream;
+            this.Start.Font = new System.Drawing.Font("Courier New", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(332, 318);
+            this.Start.Margin = new System.Windows.Forms.Padding(4);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(137, 60);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label_u
+            // 
+            this.label_u.AutoSize = true;
+            this.label_u.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_u.Location = new System.Drawing.Point(279, 228);
+            this.label_u.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_u.Name = "label_u";
+            this.label_u.Size = new System.Drawing.Size(99, 20);
+            this.label_u.TabIndex = 4;
+            this.label_u.Text = "Username:";
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(400, 228);
+            this.Username.Margin = new System.Windows.Forms.Padding(4);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(179, 22);
+            this.Username.TabIndex = 5;
+            this.Username.TextChanged += new System.EventHandler(this.Username_TextChanged);
+            // 
+            // game_name
+            // 
+            this.game_name.AutoSize = true;
+            this.game_name.Font = new System.Drawing.Font("Castellar", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.game_name.Location = new System.Drawing.Point(135, 106);
+            this.game_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.game_name.Name = "game_name";
+            this.game_name.Size = new System.Drawing.Size(640, 53);
+            this.game_name.TabIndex = 6;
+            this.game_name.Text = "Memorize The Picture";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.username_show);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.timer);
+            this.panel2.Location = new System.Drawing.Point(0, 30);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(929, 33);
+            this.panel2.TabIndex = 9;
+            this.panel2.Visible = false;
+            // 
+            // username_show
+            // 
+            this.username_show.AutoSize = true;
+            this.username_show.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_show.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.username_show.Location = new System.Drawing.Point(52, 6);
+            this.username_show.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.username_show.Name = "username_show";
+            this.username_show.Size = new System.Drawing.Size(69, 20);
+            this.username_show.TabIndex = 3;
+            this.username_show.Text = "label4";
+            // 
+            // ImageChoose
+            // 
+            this.ImageChoose.FileName = "ImageChoose";
+            this.ImageChoose.Multiselect = true;
+            // 
+            // counter
+            // 
+            this.counter.Interval = 1000;
+            this.counter.Tick += new System.EventHandler(this.counter_Tick);
+            // 
+            // image_show
+            // 
+            this.image_show.Interval = 1000;
+            this.image_show.Tick += new System.EventHandler(this.image_show_Tick);
+            // 
+            // image_destroy
+            // 
+            this.image_destroy.Interval = 600;
+            this.image_destroy.Tick += new System.EventHandler(this.image_destroy_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(916, 553);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.game_name);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label_u);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Memorize the Picture";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label_u;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Label game_name;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label username_show;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ImageChoose;
+        private System.Windows.Forms.Timer counter;
+        private System.Windows.Forms.Timer image_show;
+        private System.Windows.Forms.Timer image_destroy;
+    }
+}
+
